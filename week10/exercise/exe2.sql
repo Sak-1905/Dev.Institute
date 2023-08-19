00798040359382c3025b1d6445455ce202efa6fa
Sguheea@gmail.com <131300388+Sak-1905@users.noreply.github.com>
2023-08-19 19:23:07 +0400

    s

---
+++ week10/exercise/exe2.sql
@@ -0,0 +1,53 @@
+ALTER TABLE students
+ADD COLUMN id serial PRIMARY KEY,
+ADD COLUMN birth_date date;
+
+INSERT INTO students (id, first_name, last_name, birth_date)
+VALUES
+    (1, 'Marc', 'Benichou', '1998-11-02'),
+    (2, 'Yoan', 'Cohen', '2010-12-03'),
+    (3, 'Lea', 'Benichou', '1987-07-27'),
+    (4, 'Amelia', 'Dux', '1996-04-07'),
+    (5, 'David', 'Grez', '2003-06-14'),
+    (6, 'Omer', 'Simpson', '1980-10-03');
+
+SELECT * FROM students;
+
+SELECT first_name, last_name
+FROM students;
+
+SELECT first_name, last_name
+FROM students
+WHERE id = 2;
+
+SELECT first_name, last_name
+FROM students
+WHERE last_name = 'Benichou' AND first_name = 'Marc';
+
+SELECT first_name, last_name
+FROM students
+WHERE last_name = 'Benichou' OR first_name = 'Marc';
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name LIKE '%a%';
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name LIKE 'a%';
+
+SELECT first_name, last_name
+FROM students
+WHERE first_name LIKE '%a';
+
+SELECT first_name, last_name
+FROM students
+WHERE SUBSTRING(first_name FROM LENGTH(first_name) - 1 FOR 1) = 'a';
+
+SELECT first_name, last_name
+FROM students
+WHERE id IN (1, 3);
+
+SELECT *
+FROM students
+WHERE birth_date >= '2000-01-01';
