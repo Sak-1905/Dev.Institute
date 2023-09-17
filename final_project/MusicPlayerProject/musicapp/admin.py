2c0aaddeba4fc5dc2049ecbbaace6d14fd6a1606
Sguheea@gmail.com <131300388+Sak-1905@users.noreply.github.com>
2023-09-17 09:40:09 +0400

    s

--- final_project/MusicPlayerProject/musicapp/admin.py
+++
@@ -1,8 +0,0 @@
-# musicapp/admin.py
-
-from django.contrib import admin
-from .models import Song, Playlist, UserInteraction
-
-admin.site.register(Song)
-admin.site.register(Playlist)
-admin.site.register(UserInteraction)
--- final_project/MusicPlayerProject/musicapp/forms.py
+++
@@ -1,9 +0,0 @@
-# musicapp/forms.py
-
-from django import forms
-from .models import Song
-
-class SongUploadForm(forms.ModelForm):
-    class Meta:
-        model = Song
-        fields = ['title', 'artist', 'album', 'audio_file', 'duration']
