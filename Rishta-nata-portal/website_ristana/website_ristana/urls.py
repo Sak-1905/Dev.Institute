66e96d27a1d7e69da6e143ef5eb2624ab0dc0ada
Sguheea@gmail.com <131300388+Sak-1905@users.noreply.github.com>
2023-10-10 00:32:51 +0400

    s

--- Rishta-nata-portal/website_ristana/website_ristana/urls.py
+++ Rishta-nata-portal/website_ristana/website_ristana/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from ristana import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('ristana.urls')),
-
+    
+    path('', views.HomeView, name='home'),
+    path('login/', views.CustomLoginView.as_view(), name='login'), 
+    path('signup/', views.RegisterView.as_view(), name='signup'),
 ]
