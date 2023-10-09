399e676078b4fe0b130c74d2683c1d977e842192
Sguheea@gmail.com <131300388+Sak-1905@users.noreply.github.com>
2023-10-09 23:15:59 +0400

    s

--- Rishta-nata-portal/website_ristana/website_ristana/settings.py
+++ Rishta-nata-portal/website_ristana/website_ristana/settings.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for website_ristana project.
 
@@ -124,10 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
