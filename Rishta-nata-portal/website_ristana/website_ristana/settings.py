264f889eb4aaa79b31b239f7ae5edc88c9126b21
Sguheea@gmail.com <131300388+Sak-1905@users.noreply.github.com>
2023-10-09 11:08:58 +0400

    s

--- Rishta-nata-portal/website_ristana/website_ristana/settings.py
+++ Rishta-nata-portal/website_ristana/website_ristana/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for website_ristana project.
 
@@ -14,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
