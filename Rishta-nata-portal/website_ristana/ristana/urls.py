196772c63d1ffff13fd4ff3ae146116337dc8a63
Nomekuma <t.gunness35@gmail.com>
2023-10-09 22:53:29 +0400

    feat: update

--- Rishta-nata-portal/website_ristana/ristana/urls.py
+++ Rishta-nata-portal/website_ristana/ristana/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from ristana import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('login/', views.CustomLoginView.as_view(), name='login'), 
     path('signup/', views.RegisterView.as_view(), name='signup'),
-    path('home/', views.HomeView, name='home'),
 ]
 
